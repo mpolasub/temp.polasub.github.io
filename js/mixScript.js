bde45e5660d20229868c05d72b3bca72fae1cbf3
mpolasub <nondhawit@gmail.com>
2023-05-13 16:42:12 +0700

    sunset + added post it code

--- js/mixScript.js
+++ js/mixScript.js
@@ -880,14 +880,14 @@ link.onmouseenter = () => {
 var r1 = document.querySelector(':root');
 
 //   sunset variation
-//   r1.style.setProperty('--red1', '#fce38a');
-//   r1.style.setProperty('--darkPink1', '#fc8181');
-//   r1.style.setProperty('--pink1', '#ff8080');
+  r1.style.setProperty('--red1', '#fce38a');
+  r1.style.setProperty('--darkPink1', '#fc8181');
+  r1.style.setProperty('--pink1', '#ff8080');
 
 // default variation
-r1.style.setProperty('--red1', 'white');
-r1.style.setProperty('--darkPink1', 'aquamarine');
-r1.style.setProperty('--pink1', 'mediumpurple');
+// r1.style.setProperty('--red1', 'white');
+// r1.style.setProperty('--darkPink1', 'aquamarine');
+// r1.style.setProperty('--pink1', 'mediumpurple');
 
   r1.style.setProperty('--fourDefaultBg', 'white')
   r1.style.setProperty('--pss', '4.5rem')
